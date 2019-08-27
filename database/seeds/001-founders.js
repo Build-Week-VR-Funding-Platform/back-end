b341113c5ffef8a88f4e8ab76f83fa5759625c15
Melanie Harris <mell.j.harris@gmail.com>
2019-08-27 15:23:02 -0700

    deploying

--- database/seeds/001-founders.js
+++ database/seeds/001-founders.js
@@ -1,5 +1,5 @@
 
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
 
 exports.seed = function(knex) {
   
@@ -7,19 +7,19 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('founders').insert([
-        { username: 'Liz-Smart-Bottle', password: bcrypt.hashSync('password', 10), 
+        { username: 'Liz-Smart-Bottle', password:'Stephanie', 
         name: 'Stephanie', 
         email: 'Stephanie@founder.com', 
         about: 'My smart bottle company helps you effortlessly healthy lifestyle. With just a tap of a button LIZ destroys 99.9% of harmful viruses and odor-causing bacteria in your empty bottle or clear beverage with UV-C light sterilization. '},// hash store password in db
 
         {
           username: 'Comonsol-Grid', 
-          password: bcrypt.hashSync('password', 10), 
+          password: 'Melanie', 
           name: 'Melanie', email: 'Melanie@founder.com', 
           about: 'Comonsol has developed innovative plug-n-play technology that allows everyone everywhere to connect to cellular power grids. We are supporting an off-grid Guatemalan coffee-producing community crowdfund investment to install the worlds first cellular power grid, so they can power their future.' },
 
         { username: 'BladeX-Monitors', 
-        password: bcrypt.hashSync('password', 10), 
+        password: 'Areum', 
         name: 'Areum', email: 'Areum@founder.com', 
         about: 'BladeX is one of the most lightweight and compact on-the-go external monitors available today. At under 2 pounds, it is easy to store and travel with. It’s available in 1080P and 4K high definition versions and this 15.6” monitor promises to boost your productivity by up to 50%.' },
         
